0e18a50af3fce04fa39c46f691b0d5efa94104ed
Rima <rsingh142005@gmail.com>
2026-07-07 20:21:26 +0530

    Add difficulty distribution chart

--- app.py
+++ app.py
@@ -13,3 +13,25 @@ df = pd.read_csv("data/problems.csv")
 
 st.subheader("Solved Problems")
 st.dataframe(df)
+
+
+# Statistics
+total = len(df)
+
+easy = len(df[df["Difficulty"] == "Easy"])
+medium = len(df[df["Difficulty"] == "Medium"])
+hard = len(df[df["Difficulty"] == "Hard"])
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric("Total", total)
+col2.metric("Easy", easy)
+col3.metric("Medium", medium)
+col4.metric("Hard", hard)
+
+# Difficulty Distribution Chart
+difficulty_counts = df["Difficulty"].value_counts()
+
+st.subheader("Difficulty Distribution")
+
+st.bar_chart(difficulty_counts)
